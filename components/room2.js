11943dc28114899ed060f9e0f6b3441d255ca1be
FitoreO <fitoreosmanaj@gmail.com>
2019-04-05 12:57:39 +0200

    newaudioplayer

--- components/room2.js
+++ components/room2.js
@@ -6,8 +6,12 @@ import {
   View,
   Environment,
   asset,
+  Sound,
+  NativeModules,
 } from 'react-360';
+const { AudioModule } = NativeModules;
 Environment.preloadBackgroundImage(asset('room3_o.jpg'))
+
 
 export default class Room2 extends React.Component {
 
@@ -19,14 +23,25 @@ export default class Room2 extends React.Component {
       }
     );
 
+    AudioModule.playEnvironmental({
+      source: asset('Billiesong.mp3'),
+      volume: 0.1, // play at 3/10 original volume
+
+    });
+  }
+
+  componentWillUnmount() {
+    AudioModule.stopEnvironmental()
   }
   render() {
     return (
       <View style={styles.panel}>
+        {/* <Sound source={'Billiesong.mp3'}></Sound> */}
         <View style={styles.greetingBox}>
           <Text style={styles.greeting2}>
             Köket
           </Text>
+
         </View>
       </View>
     );
